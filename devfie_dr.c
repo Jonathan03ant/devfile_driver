d13bc5f0e7262a8ee8a76c56e73104ec901df43e
jonathan0 <jonathan03ant@gmail.com>
2024-06-03 04:33:42 +0100

    devfile open() and close() functions

--- devfie_dr.c
+++ devfie_dr.c
@@ -9,6 +9,19 @@
 
 
 
+/*
+ * When a user application calls open on a device file, the kernel will forward the call to this function.
+ * inode dev_file is needed for the actual driver to determine permission, check major and minor number etc
+ * the file instance is created after the device file is opned.
+ */
+
+static int devfile_open ( struct inode *dev_file, struct file *opened_instance ) {
+
+};
+
+
+static int devfile_close (struct inode *devfile, struct file *instance){
+};
 
 
 
