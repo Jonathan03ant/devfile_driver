c80d45781e40886e5fcde0615691cac5e8653bdd
jonathan <jonathan03ant@gmail.com>
2024-06-04 13:57:36 -0400

    commit #3

--- devfie_dr.c
+++ devfie_dr.c
@@ -3,18 +3,16 @@
 #include <linux/fs.h>
 
 
-
-
-
+# define MYMAJOR 19; //This number is defined for the device file's major number when we register it
 
 
 
 /*
+ 	*@NOTE 
  * When a user application calls open on a device file, the kernel will forward the call to this function.
  * inode dev_file is needed for the actual driver to determine permission, check major and minor number etc
  * the file instance is created after the device file is opned.
  */
-
 
 static struct file_operation fops = {
 	.owner = THIS_MODULE, 
@@ -22,14 +20,14 @@ static struct file_operation fops = {
 	.release = devFileRelease
 };
 
-static int defFileOpen (struct inode *deviceFile, struct file *instance){
+static int defFileOpen(struct inode *deviceFile, struct file *instance){
 	printk("device driver -- open was called");
 	return 0;
 
 };
 
 
-static int defFileRelease ( truct inode *deviceFile, struct file * instance){
+static int defFileRelease (struct inode *deviceFile, struct file *instance){
 	printk("device driver -- close was called\n");
 	return 0;
 };
@@ -42,9 +40,33 @@ static int __init moduleInit(void){
 	 * registering the dev file with major and minor numbers
 	 */
 
+
+
 };
 
-static void __exit moduleExit(void){};
+
+static void __exit moduleExit(void){
+	/*
+	 * Unregister the register device
+	 * print something
+	 */
+
+	printk("Hello from Kernel");
+	int success;
+
+	/*
+	 * Register device nummber
+	 */
+	success = register_chrdev(MYMAJOR, "joesDeviceFile", &fops);
+
+	if (success == 0){
+	
+	} else if (success > 0) {
+
+	} else {
+
+	};
+};
 
 
 
